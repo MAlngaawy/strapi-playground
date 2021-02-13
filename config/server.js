6ef2a44b6e95a044e747ac5ea76244d62f0c9de9
MAlngaawy <m.alngaawy@gmail.com>
2021-02-13 14:55:45 +0200

    initial the app

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ebba905f66b96928968836c38665425f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '51124af8-ca03-425f-9fd8-72c6de9c64d1'
+};
